2d0b629328a65cebb1ae2972a401e3542bd246d9
hectorParamio <hectorparamio@gmail.com>
2024-12-20 20:06:17 +0100

    este el trabajo lol

---
+++ Assets/ZMine/Scripts/EndPlatform.cs
@@ -0,0 +1,63 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+public class EndPlatform : UdonSharpBehaviour
+{
+    public GameObject gunPrefab; // Assign your gun model in Unity Inspector
+    private int remainingShots = 3;
+    private bool hasGun = false;
+    private VRCPlayerApi localPlayer;
+
+    void Start()
+    {
+        localPlayer = Networking.LocalPlayer;
+    }
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        if (player == localPlayer && !hasGun)
+        {
+            hasGun = true;
+            remainingShots = 3;
+            // You would typically instantiate or enable your gun model here
+            if (gunPrefab != null)
+            {
+                gunPrefab.SetActive(true);
+            }
+        }
+    }
+
+    public override void InputUse(bool value, VRC.Udon.Common.UdonInputEventArgs args)
+    {
+        if (!hasGun || !value || remainingShots <= 0) return;
+
+        bool isVR = localPlayer.IsUserInVR();
+        bool shouldShoot = (isVR && args.handType == VRC.Udon.Common.HandType.RIGHT)
+                          || (!isVR && args.boolValue);
+
+        if (shouldShoot)
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        if (remainingShots > 0)
+        {
+            // Add shooting logic here (particle effects, raycasts, etc.)
+            remainingShots--;
+
+            if (remainingShots <= 0)
+            {
+                // Disable gun when out of ammo
+                if (gunPrefab != null)
+                {
+                    gunPrefab.SetActive(false);
+                }
+                hasGun = false;
+            }
+        }
+    }
+}
---
+++ Assets/ZMine/Scripts/PlatformToggle.cs
@@ -0,0 +1,32 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+public class PlatformToggle : UdonSharpBehaviour
+{
+    private void Start()
+    {
+        Transform leftPlatform = transform.Find("JumpPlatformL");
+        Transform rightPlatform = transform.Find("JumpPlatformR");
+
+        if (leftPlatform != null && rightPlatform != null)
+        {
+            int randomChoice = Random.Range(0, 2);
+            Collider leftCollider = leftPlatform.GetComponent<Collider>();
+            Collider rightCollider = rightPlatform.GetComponent<Collider>();
+
+            if (leftCollider != null && rightCollider != null)
+            {
+                if (randomChoice == 0)
+                {
+                    leftCollider.enabled = true;
+                    rightCollider.enabled = false;
+                }
+                else
+                {
+                    leftCollider.enabled = false;
+                    rightCollider.enabled = true;
+                }
+            }
+        }
+    }
+}
