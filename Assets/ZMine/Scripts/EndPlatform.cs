377d3a00683096e145aac795e530a3178cdfa914
hectorParamio <hectorparamio@gmail.com>
2024-12-20 21:18:25 +0100

    estoy intento equipar el arma al tocar la plataforma de salida

--- Assets/ZMine/Scripts/EndPlatform.cs
+++ Assets/ZMine/Scripts/EndPlatform.cs
@@ -4,60 +4,77 @@ using VRC.SDKBase;
 
 public class EndPlatform : UdonSharpBehaviour
 {
-    public GameObject gunPrefab; // Assign your gun model in Unity Inspector
-    private int remainingShots = 3;
+    public GameObject gunPrefab;
+    public ParticleSystem muzzleFlash;
+    public AudioSource shootSound;
     private bool hasGun = false;
     private VRCPlayerApi localPlayer;
+    private GameObject activeGun;
+    private bool isVR = false;
 
     void Start()
     {
+        Debug.Log("[EndPlatform] Script started");
         localPlayer = Networking.LocalPlayer;
+        isVR = localPlayer.IsUserInVR();
+        
+        if (gunPrefab != null)
+        {
+            activeGun = VRCInstantiate(gunPrefab);
+            activeGun.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+            activeGun.SetActive(false);
+            Debug.Log("[EndPlatform] Gun created");
+        }
     }
 
     public override void OnPlayerTriggerEnter(VRCPlayerApi player)
     {
+        Debug.Log("[EndPlatform] Trigger entered by player");
         if (player == localPlayer && !hasGun)
         {
+            Debug.Log("[EndPlatform] Local player entered trigger");
+            GiveGunToPlayer();
+        }
+    }
+
+    void GiveGunToPlayer()
+    {
+        if (activeGun != null)
+        {
             hasGun = true;
-            remainingShots = 3;
-            // You would typically instantiate or enable your gun model here
-            if (gunPrefab != null)
-            {
-                gunPrefab.SetActive(true);
-            }
+            activeGun.SetActive(true);
+            Debug.Log("[EndPlatform] Gun activated");
         }
     }
 
-    public override void InputUse(bool value, VRC.Udon.Common.UdonInputEventArgs args)
+    void Update()
     {
-        if (!hasGun || !value || remainingShots <= 0) return;
-
-        bool isVR = localPlayer.IsUserInVR();
-        bool shouldShoot = (isVR && args.handType == VRC.Udon.Common.HandType.RIGHT)
-                          || (!isVR && args.boolValue);
-
-        if (shouldShoot)
+        if (hasGun && activeGun != null)
         {
-            Shoot();
+            UpdateGunPosition();
         }
     }
 
-    private void Shoot()
+    void UpdateGunPosition()
     {
-        if (remainingShots > 0)
+        if (isVR)
         {
-            // Add shooting logic here (particle effects, raycasts, etc.)
-            remainingShots--;
-
-            if (remainingShots <= 0)
-            {
-                // Disable gun when out of ammo
-                if (gunPrefab != null)
-                {
-                    gunPrefab.SetActive(false);
-                }
-                hasGun = false;
-            }
+            // VR hand tracking
+            Vector3 handPosition = localPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.RightHand).position;
+            Quaternion handRotation = localPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.RightHand).rotation;
+            
+            activeGun.transform.position = handPosition;
+            activeGun.transform.rotation = handRotation * Quaternion.Euler(0, 90, 0);
+        }
+        else
+        {
+            // Desktop mode - follow camera
+            Vector3 playerPosition = localPlayer.GetPosition();
+            Quaternion playerRotation = localPlayer.GetRotation();
+            
+            Vector3 offset = playerRotation * new Vector3(0.3f, 1.2f, 0.5f);
+            activeGun.transform.position = playerPosition + offset;
+            activeGun.transform.rotation = playerRotation * Quaternion.Euler(0, 90, 0);
         }
     }
 }
