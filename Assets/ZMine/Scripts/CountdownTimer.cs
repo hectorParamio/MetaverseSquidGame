01b86c1f455b910700ce04e6dd4ff210a2f56070
hectorParamio <hectorparamio@gmail.com>
2024-12-27 20:19:43 +0100

    Temporizador
    Lógica con panel visual. Falta hacer que cuando acabe el tiempo ocurran. Por defecto empieza a contar desde el inicio del mundo

---
+++ Assets/ZMine/Scripts/CountdownTimer.cs
@@ -0,0 +1,38 @@
+﻿using UdonSharp;
+using UnityEngine;
+using TMPro;
+
+public class CountdownTimer : UdonSharpBehaviour
+{
+    public TextMeshProUGUI timerText;
+    public float countdownTime = 300f;
+
+    private float timeRemaining;
+
+    void Start()
+    {
+        timeRemaining = countdownTime;
+        UpdateTimerDisplay();
+    }
+
+    void Update()
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            UpdateTimerDisplay();
+        }
+        else
+        {
+            timeRemaining = 0;
+            UpdateTimerDisplay();
+        }
+    }
+
+    void UpdateTimerDisplay()
+    {
+        int minutes = Mathf.FloorToInt(timeRemaining / 60);
+        int seconds = Mathf.FloorToInt(timeRemaining % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
